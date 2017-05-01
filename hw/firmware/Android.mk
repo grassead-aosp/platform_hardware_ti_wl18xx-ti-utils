dd1bafa6b69d3f1b218c2d23cc4443781d4b7b89
varigigi <pierluigi.p@variscite.com>
2017-05-01 14:53:27 +0200

    disable old TI firmware installation

--- hw/firmware/Android.mk
+++ hw/firmware/Android.mk
@@ -1,6 +1,9 @@
 local_target_dir := $(TARGET_OUT)/etc/firmware/ti-connectivity
 LOCAL_PATH:= $(call my-dir)
 
+INSTALL_TI_FIRMWARE := false
+ifneq ($(INSTALL_TI_FIRMWARE),false)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := wl1271-nvs.bin
 LOCAL_MODULE_TAGS := debug
@@ -56,3 +59,5 @@ LOCAL_MODULE_PATH := $(local_target_dir)
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 LOCAL_MODULE_CLASS := FIRMWARE
 include $(BUILD_PREBUILT)
+
+endif
